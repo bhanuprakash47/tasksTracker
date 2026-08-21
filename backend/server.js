ec624b61a533f1b8850c58e064eb6395b2804b24
bhanuprakash47 <bhanuprakashnampally54@gmail.com>
2026-08-21 15:48:22 +0530

    updated backend port

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use("/api/auth",authRoutes)
 app.use("/api/tasks",taskRoutes)
 
 const MONGO_URL=process.env.MONGO_URL
-const PORT =process.env.PORT
+const PORT =process.env.PORT || 5000
 
 const initializeServerAndDB=async()=>{
     try{
